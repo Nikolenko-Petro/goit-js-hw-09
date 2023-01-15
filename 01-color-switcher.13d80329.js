2fbced15ad542b289c168a566c44032b3d7bb96c
Nikolenko-Petro <115504876+Nikolenko-Petro@users.noreply.github.com>
2023-01-15 13:36:25 +0000

    Deploying to gh-pages from @ Nikolenko-Petro/goit-js-hw-09@b6f1bdf15ee9448f3c10915ec84c04eceafd4f35 🚀

---
+++ 01-color-switcher.13d80329.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.13d80329.js.map
